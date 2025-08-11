b1afbf5c7a5117584f55d31f1db0245b7d2e6931
czm100803 <111917925+czm100803@users.noreply.github.com>
2025-08-10 20:43:33 -0500

    Update television.py

--- television.py
+++ television.py
@@ -1,27 +1,33 @@
 
 
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    """A simple television remote emulator that has power, volume, and mute"""
+
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL:int  = 3
 
     def __init__(self):
+        """used to initialize the different tv settings"""
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__status = False
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        """allows for on and off of the TV"""
         self.__status = not self.__status
 
 
     def mute(self):
+        """allows for the ability to mute the TV"""
         if self.__status:
             self.__muted = not self.__muted
 
 
     def channel_up(self):
+        """allows to change the channel up by one"""
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -29,6 +35,7 @@ class Television:
                 self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        """allows to change the channel down by one"""
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -36,20 +43,21 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
+        """allows to change the volume up by one"""
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
-                if self.__muted:
-                    self.__muted = False
                 self.__volume += 1
 
     def volume_down(self):
+        """allows to change the volume down by one"""
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
     def __str__(self):
+        """Returns a string that displays the TV Status"""
         if self.__muted:
             self.__muted = 0
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__muted}'
